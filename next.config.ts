6f63e8488bf2a18715112ec41f20a60a61d59b96
Sai Nakul Reddy Manne <sai@mac.mynetworksettings.com>
2025-09-06 20:40:38 -0400

    Next Config update

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",             // generates static HTML
-  basePath: "/Portfolio_Website", // your GitHub repo name
-  images: {
-    unoptimized: true,          // disable server-side image optimization
-  },
-  eslint: {
-    ignoreDuringBuilds: true,   // ✅ disable ESLint checks during build
-  },
+  /* config options here */
+  output: "export",
 };
 
 export default nextConfig;
